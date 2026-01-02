c6e9e2841450a82817edb4a79fccb8d301f69e21
Amirhossein Emadi <emadi.henker404@gmail.com>
2026-01-02 12:28:02 +0330

    :sparkles: feat(comments): add w3schools multi-line comments example

---
+++ syntax/comments/w3schools/ex3/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+		The code below will print Multi-line comments!
+		to the screen, and it is amazing
+	*/
+	fmt.Println("Multi-line comments!")
+}
