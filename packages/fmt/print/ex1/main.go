b9724d92d69e90f1461bfc5bdbe885b858e4cdbf
Amirhossein Emadi <emadi.henker404@gmail.com>
2026-01-02 13:14:32 +0330

    :sparkles: feat(fmt/print): add example for fmt.Print function (#4)

---
+++ packages/fmt/print/ex1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name, age = "Amirhossein", 25
+	fmt.Print(name, " is ", age, " years old.\n")
+
+	// It is conventional not to worry about any error returned by Print.
+
+}
